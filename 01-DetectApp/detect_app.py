b184aec5b327e072c555668dd02d781940f34f0c
thirsd <thirsd.sina.com>
2019-01-20 21:12:07 +0800

    computer the tf-idf of apps and hosts

--- 01-DetectApp/detect_app.py
+++ 01-DetectApp/detect_app.py
@@ -1,11 +1,12 @@
 #coding:utf-8
 
 import os
+import copy
 import sys
 from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
-
+import pandas as pd
 class DetectApp(object):
     ''' detect which host in the app is deployed
 
@@ -15,11 +16,11 @@ class DetectApp(object):
         self.app_file_dir = app_file_dir
         self.host_file_dir = host_file_dir
 
-        # init the sklearn's class to cut list to words
-        self.vectorizer = CountVectorizer(token_pattern=r"(?u)\b[^/\n]+\b")
-        # init the sklearn's class to compute the tf-idf weight about words
-        self.transformer = TfidfTransformer()
-        self.tfidf = None
+        self.app_ids, self.app_vectorizer, self.app_tfidf  \
+            = self._build_tfidf_(self.app_file_dir)
+
+        self.host_ids, self.host_vectorizer, self.host_tfidf \
+            = self._build_tfidf_(self.host_file_dir)
 
     def _read_files_(self, file_dir):
         file_content = {}
@@ -30,22 +31,39 @@ class DetectApp(object):
                 file_content[file] = '/'.join(fd.readlines())
         return file_content
 
-    def _build_tfidf_(self):
-        apps_cotent = self._read_files_(self.app_file_dir)
-        print(apps_cotent.values())
-        tfidf = self.transformer.fit_transform(
-            self.vectorizer.fit_transform(apps_cotent.values()))
-        return tfidf
+    def _build_tfidf_(self, file_dir):
+        content = self._read_files_(file_dir)
+        ids = content.keys()
+        # init the sklearn's class to cut list to words
+        vectorizer = CountVectorizer(token_pattern=r"(?u)\b[^/\n]+\b")
+        # init the sklearn's class to compute the tf-idf weight about words
+        transformer = TfidfTransformer()
+        #print(cotent.values())
+        tfidf = transformer.fit_transform(
+            vectorizer.fit_transform(content.values()))
+        return ids, vectorizer, tfidf
 
-    def show_tfidf(self):
-        word = self.vectorizer.get_feature_names()
-        weight = self.tfidf.toarray()
-        for i in range(len(weight)):
-            print(u"-------这里输出第", i, u"类文本的词语tf-idf权重------")
-            for j in range(len(word)):
-                print("%s:%s" % (word[j], weight[i][j]))
+    def _show_tfidf_(self, vectorizer, tfidf):
+        words = vectorizer.get_feature_names()
+        #print(words)
 
-    def _cosine_distance_(v1, v2):
+        #weight = tfidf.toarray()
+        #for i in range(len(weight)):
+        #    print(weight[i])
+            #print(u"-------这里输出第", i, u"类文本的词语tf-idf权重------")
+            #for j in range(len(word)):
+            #    print("%s:%s" % (word[j], weight[i][j]))
+        #print(type(tfidf))
+        df_tfidf = pd.DataFrame(tfidf.todense(), columns=words)
+        print(df_tfidf.T)
+
+    def show_tfidf(self, type="app"):
+        if type=="host":
+            self._show_tfidf_(self.host_vectorizer, self.host_tfidf)
+        else:
+            self._show_tfidf_(self.app_vectorizer, self.app_tfidf)
+
+    def cosine_distance(self, v1, v2):
         dot_product = 0.0
         sqr_v1 = 0.0
         sqr_v2 = 0.0
@@ -59,12 +77,20 @@ class DetectApp(object):
             return dot_product / ((sqr_v1 * sqr_v2) ** 0.5)
 
     def detect_app(self):
-        self.tfidf = self._build_tfidf_()
+        df_app_tfidf = pd.DataFrame(self.app_tfidf.todense(),
+                                    columns=self.app_vectorizer.get_feature_names()).T
+        set_app_words_org = self.app_vectorizer.get_feature_names()
+        ser_app_tfidf_median = df_app_tfidf.median()
 
+        for index, val in ser_app_tfidf_median.items():
+            set_app_words
+
+        pass
 
 
 if __name__ == "__main__":
     detector = DetectApp("./data/apps/", "./data/hosts/")
     detector.detect_app()
-    detector.show_tfidf()
+    detector.show_tfidf(type="app")
+    detector.show_tfidf(type="host")
 
